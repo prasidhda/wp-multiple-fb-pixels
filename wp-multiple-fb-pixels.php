b598747d0fd354297780d18a9eb4460d19dac9c4
Prasidhda Malla <pranuk.prwnsi@gmail.com>
2018-04-23 10:35:08 +0545

    Initial Push.

---
+++ wp-multiple-fb-pixels.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: WP Multiple FB Pixels - User Defined Multipple FB Pixels Event Tracking
+Plugin URI:   https://github.com/prasidhda/wp-multiple-fb-pixels
+Description:  WP plugin to allow multiple FB pixel events tracking
+Version:      1.0.0
+Author:       prasidhda
+Author URI:   https://profiles.wordpress.org/prasidhda
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  wporg
+Domain Path:  /languages
+*/
+
